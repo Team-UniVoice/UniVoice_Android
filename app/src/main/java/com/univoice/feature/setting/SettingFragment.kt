d1bef627cf2737d6064148738d22fda4280eb69c
Eonji-sw <ejkim0625@gmail.com>
2024-07-01 17:02:54 +0900

    #4 [FEAT] : SettingFragment 생성

---
+++ app/src/main/java/com/univoice/feature/setting/SettingFragment.kt
@@ -0,0 +1,9 @@
+package com.univoice.feature.setting
+
+import com.univoice.R
+import com.univoice.core_ui.base.BindingFragment
+import com.univoice.databinding.FragmentHomeBinding
+
+class SettingFragment : BindingFragment<FragmentHomeBinding>(R.layout.fragment_setting) {
+    override fun initView() {}
+}
