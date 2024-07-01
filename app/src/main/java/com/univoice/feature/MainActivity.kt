4b0422266494a5cc6223994df240bd0658ae1f6b
Eonji-sw <ejkim0625@gmail.com>
2024-07-01 17:02:32 +0900

    #4 [FEAT] : bottom nav 구현

--- app/src/main/java/com/univoice/feature/MainActivity.kt
+++ app/src/main/java/com/univoice/feature/MainActivity.kt
@@ -1,9 +1,43 @@
 package com.univoice.feature
 
+import android.view.View
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupWithNavController
 import com.univoice.R
 import com.univoice.core_ui.base.BindingActivity
 import com.univoice.databinding.ActivityMainBinding
 
 class MainActivity : BindingActivity<ActivityMainBinding>(R.layout.activity_main) {
-    override fun initView() {}
+    override fun initView() {
+        initMainBottomNavigation()
+    }
+
+    private fun initMainBottomNavigation() {
+        val navController = (supportFragmentManager.findFragmentById(R.id.fcv_main_nav) as NavHostFragment).findNavController()
+        binding.bnvMainNav.apply {
+            setupWithNavController(navController)
+            itemIconTintList = null
+        }
+
+        setBottomNavigationVisible(navController)
+    }
+
+    private fun setBottomNavigationVisible(navController: NavController) {
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            binding.bnvMainNav.visibility =
+                if (destination.id in
+                    listOf(
+                        R.id.fragment_home,
+                        R.id.fragment_storage,
+                        R.id.fragment_setting
+                    )
+                ) {
+                    View.VISIBLE
+                } else {
+                    View.GONE
+                }
+        }
+    }
 }
