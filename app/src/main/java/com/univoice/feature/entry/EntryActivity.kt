65088b47bbb6c8abcf49b3bbf1a371e52d0b853c
gaeulzzang <gaeulzzang@gmail.com>
2024-07-04 00:16:41 +0900

    [UI] : 시작하기 페이지 뷰 구현

--- app/src/main/java/com/univoice/feature/entry/EntryActivity.kt
+++ app/src/main/java/com/univoice/feature/entry/EntryActivity.kt
@@ -1,5 +1,6 @@
 package com.univoice.feature.entry
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -8,10 +9,18 @@ import androidx.core.view.WindowInsetsCompat
 import com.univoice.R
 import com.univoice.core_ui.base.BindingActivity
 import com.univoice.databinding.ActivityEntryBinding
+import com.univoice.feature.login.LoginActivity
 
 class EntryActivity : BindingActivity<ActivityEntryBinding>(R.layout.activity_entry) {
     override fun initView() {
-        TODO("Not yet implemented")
+        initLoginBtnClickListener()
+    }
+
+    private fun initLoginBtnClickListener() {
+        binding.btnEntryLogin.setOnClickListener {
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
     }
 
 }
---
+++ app/src/main/java/com/univoice/feature/login/LoginActivity.kt
@@ -0,0 +1,21 @@
+package com.univoice.feature.login
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.univoice.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_login)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
