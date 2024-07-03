62b34401274fe802f67fb1af4436c8d02f3e17ca
gaeulzzang <gaeulzzang@gmail.com>
2024-07-04 01:20:21 +0900

    [UI] : 로그인 페이지 뷰 구현

--- app/src/main/java/com/univoice/feature/login/LoginActivity.kt
+++ app/src/main/java/com/univoice/feature/login/LoginActivity.kt
@@ -1,21 +1,26 @@
 package com.univoice.feature.login
 
-import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
-import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import com.univoice.R
+import com.univoice.core_ui.base.BindingActivity
+import com.univoice.databinding.ActivityLoginBinding
 
-class LoginActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_login)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+class LoginActivity : BindingActivity<ActivityLoginBinding>(R.layout.activity_login) {
+    override fun initView() {
+        initToolbar()
+        initToolbarClickListener()
+    }
+
+    private fun initToolbar() {
+        with(binding) {
+            setSupportActionBar(toolbar)
+            supportActionBar?.setDisplayShowTitleEnabled(false)
+            tvToolbarTitle.text = getString(R.string.login_toolbar_title)
+        }
+    }
+
+    private fun initToolbarClickListener() {
+        binding.toolbar.setNavigationOnClickListener {
+            finish()
         }
     }
 }
