194f883410984710f5b856e80cd07abcaa76e986
gaeulzzang <gaeulzzang@gmail.com>
2024-07-03 23:43:36 +0900

    [UI] : 시작하기 화면 xml 새성

---
+++ app/src/main/java/com/univoice/feature/entry/EntryActivity.kt
@@ -0,0 +1,17 @@
+package com.univoice.feature.entry
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.univoice.R
+import com.univoice.core_ui.base.BindingActivity
+import com.univoice.databinding.ActivityEntryBinding
+
+class EntryActivity : BindingActivity<ActivityEntryBinding>(R.layout.activity_entry) {
+    override fun initView() {
+        TODO("Not yet implemented")
+    }
+
+}
