08046776fb2e1bd9150e7789322b3ccec17e1473
Eonji-sw <ejkim0625@gmail.com>
2024-07-04 22:25:35 +0900

    #15 [FEAT] : HomeViewModel 구현

---
+++ app/src/main/java/com/univoice/feature/home/HomeViewModel.kt
@@ -0,0 +1,299 @@
+package com.univoice.feature.home
+
+import androidx.lifecycle.ViewModel
+import com.univoice.domain.entity.NoticeCategoryListEntity
+import com.univoice.domain.entity.NoticeListEntity
+import com.univoice.domain.entity.QuickScanListEntity
+
+class HomeViewModel : ViewModel() {
+    val mockQuickScanList = listOf(
+        QuickScanListEntity(
+            name = "아주대학교\n총학생회",
+            image = "",
+            count = 5
+        ),
+        QuickScanListEntity(
+            name = "자연과학대학\n학생회",
+            image = "",
+            count = 3
+        ),
+        QuickScanListEntity(
+            name = "경영대학\n학생회",
+            image = "",
+            count = 5
+        ),
+        QuickScanListEntity(
+            name = "인문대학\n학생회",
+            image = "",
+            count = 4
+        ),
+        QuickScanListEntity(
+            name = "예술대학\n학생회",
+            image = "",
+            count = 1
+        ),
+    )
+
+    val mockNoticeList = listOf(
+        NoticeCategoryListEntity(
+            name = "전체",
+            noticeData = listOf(
+                NoticeListEntity(
+                    subTitle = "공지사항",
+                    title = "전체1",
+                    date = "06/26 ~ 07/01",
+                    like = 10,
+                    views = 8,
+                    image = true,
+                ),
+                NoticeListEntity(
+                    subTitle = "공지사항",
+                    title = "전체2",
+                    date = "06/26 ~ 07/01",
+                    like = 10,
+                    views = 8,
+                    image = false,
+                ),
+                NoticeListEntity(
+                    subTitle = "공지사항",
+                    title = "전체3",
+                    date = "06/26 ~ 07/01",
+                    like = 10,
+                    views = 8,
+                    image = true,
+                ),
+                NoticeListEntity(
+                    subTitle = "공지사항",
+                    title = "전체4",
+                    date = "06/26 ~ 07/01",
+                    like = 10,
+                    views = 8,
+                    image = true,
+                ),
+                NoticeListEntity(
+                    subTitle = "공지사항",
+                    title = "전체5",
+                    date = "06/26 ~ 07/01",
+                    like = 10,
+                    views = 8,
+                    image = false,
+                ),
+                NoticeListEntity(
+                    subTitle = "공지사항",
+                    title = "전체6",
+                    date = "06/26 ~ 07/01",
+                    like = 10,
+                    views = 8,
+                    image = true,
+                ),
+                NoticeListEntity(
+                    subTitle = "공지사항",
+                    title = "전체7",
+                    date = "06/26 ~ 07/01",
+                    like = 10,
+                    views = 8,
+                    image = false,
+                ),
+                NoticeListEntity(
+                    subTitle = "공지사항",
+                    title = "전체8",
+                    date = "06/26 ~ 07/01",
+                    like = 10,
+                    views = 8,
+                    image = true,
+                ),
+                NoticeListEntity(
+                    subTitle = "공지사항",
+                    title = "전체9",
+                    date = "06/26 ~ 07/01",
+                    like = 10,
+                    views = 8,
+                    image = true,
+                ),
+                NoticeListEntity(
+                    subTitle = "공지사항",
+                    title = "전체10",
+                    date = "06/26 ~ 07/01",
+                    like = 10,
+                    views = 8,
+                    image = true,
+                ),
+                NoticeListEntity(
+                    subTitle = "공지사항",
+                    title = "전체11",
+                    date = "06/26 ~ 07/01",
+                    like = 10,
+                    views = 8,
+                    image = false,
+                ),
+            )
+        ),
+        NoticeCategoryListEntity(
+            name = "총학생회",
+            noticeData = listOf(
+                NoticeListEntity(
+                    subTitle = "공지사항",
+                    title = "총학생회1",
+                    date = "06/26 ~ 07/01",
+                    like = 10,
+                    views = 8,
+                    image = true,
+                ),
+                NoticeListEntity(
+                    subTitle = "공지사항",
+                    title = "총학생회2",
+                    date = "06/26 ~ 07/01",
+                    like = 10,
+                    views = 8,
+                    image = false,
+                ),
+                NoticeListEntity(
+                    subTitle = "공지사항",
+                    title = "총학생회3",
+                    date = "06/26 ~ 07/01",
+                    like = 10,
+                    views = 8,
+                    image = true,
+                ),
+                NoticeListEntity(
+                    subTitle = "공지사항",
+                    title = "총학생회4",
+                    date = "06/26 ~ 07/01",
+                    like = 10,
+                    views = 8,
+                    image = true,
+                ),
+                NoticeListEntity(
+                    subTitle = "공지사항",
+                    title = "총학생회5",
+                    date = "06/26 ~ 07/01",
+                    like = 10,
+                    views = 8,
+                    image = false,
+                ),
+                NoticeListEntity(
+                    subTitle = "공지사항",
+                    title = "총학생회6",
+                    date = "06/26 ~ 07/01",
+                    like = 10,
+                    views = 8,
+                    image = false,
+                ),
+                NoticeListEntity(
+                    subTitle = "공지사항",
+                    title = "총학생회7",
+                    date = "06/26 ~ 07/01",
+                    like = 10,
+                    views = 8,
+                    image = false,
+                ),
+                NoticeListEntity(
+                    subTitle = "공지사항",
+                    title = "총학생회8",
+                    date = "06/26 ~ 07/01",
+                    like = 10,
+                    views = 8,
+                    image = true,
+                ),
+            )
+        ),
+        NoticeCategoryListEntity(
+            name = "자연과학대학 학생회",
+            noticeData = listOf(
+                NoticeListEntity(
+                    subTitle = "공지사항",
+                    title = "자연과학대학 학생회1",
+                    date = "06/26 ~ 07/01",
+                    like = 10,
+                    views = 8,
+                    image = true,
+                ),
+                NoticeListEntity(
+                    subTitle = "공지사항",
+                    title = "자연과학대학 학생회2",
+                    date = "06/26 ~ 07/01",
+                    like = 10,
+                    views = 8,
+                    image = false,
+                ),
+                NoticeListEntity(
+                    subTitle = "공지사항",
+                    title = "자연과학대학 학생회3",
+                    date = "06/26 ~ 07/01",
+                    like = 10,
+                    views = 8,
+                    image = true,
+                ),
+            )
+        ),
+        NoticeCategoryListEntity(
+            name = "경영대학 학생회",
+            noticeData = listOf(
+                NoticeListEntity(
+                    subTitle = "공지사항",
+                    title = "경영대학 학생회1",
+                    date = "06/26 ~ 07/01",
+                    like = 10,
+                    views = 8,
+                    image = true,
+                ),
+                NoticeListEntity(
+                    subTitle = "공지사항",
+                    title = "경영대학 학생회2",
+                    date = "06/26 ~ 07/01",
+                    like = 10,
+                    views = 8,
+                    image = false,
+                ),
+                NoticeListEntity(
+                    subTitle = "공지사항",
+                    title = "경영대학 학생회3",
+                    date = "06/26 ~ 07/01",
+                    like = 10,
+                    views = 8,
+                    image = true,
+                ),
+                NoticeListEntity(
+                    subTitle = "공지사항",
+                    title = "경영대학 학생회4",
+                    date = "06/26 ~ 07/01",
+                    like = 10,
+                    views = 8,
+                    image = true,
+                ),
+                NoticeListEntity(
+                    subTitle = "공지사항",
+                    title = "경영대학 학생회5",
+                    date = "06/26 ~ 07/01",
+                    like = 10,
+                    views = 8,
+                    image = true,
+                ),
+                NoticeListEntity(
+                    subTitle = "공지사항",
+                    title = "경영대학 학생회6",
+                    date = "06/26 ~ 07/01",
+                    like = 10,
+                    views = 8,
+                    image = false,
+                ),
+                NoticeListEntity(
+                    subTitle = "공지사항",
+                    title = "경영대학 학생회7",
+                    date = "06/26 ~ 07/01",
+                    like = 10,
+                    views = 8,
+                    image = true,
+                ),
+                NoticeListEntity(
+                    subTitle = "공지사항",
+                    title = "경영대학 학생회8",
+                    date = "06/26 ~ 07/01",
+                    like = 10,
+                    views = 8,
+                    image = true,
+                ),
+            )
+        ),
+    )
+}
