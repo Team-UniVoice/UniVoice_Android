fbcfbe65aa5295289cc95697d79b91d414c761bd
gaeulzzang <gaeulzzang@gmail.com>
2024-07-05 12:21:28 +0900

    [UI] : Quick Scan xml 추가

---
+++ app/src/main/java/com/univoice/feature/quickscan/QuickScanActivity.kt
@@ -0,0 +1,17 @@
+package com.univoice.feature.quickscan
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.univoice.R
+import com.univoice.core_ui.base.BindingActivity
+import com.univoice.databinding.ActivityQuickScanBinding
+
+class QuickScanActivity : BindingActivity<ActivityQuickScanBinding>(R.layout.activity_quick_scan) {
+    override fun initView() {
+        TODO("Not yet implemented")
+    }
+
+}
