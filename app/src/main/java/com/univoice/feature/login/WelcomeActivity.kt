5d5c1179b7f1f65368232fcd8fccffa215fdd2f8
gaeulzzang <gaeulzzang@gmail.com>
2024-07-11 01:19:15 +0900

    #12 [MOD] : 툴바 삭제

--- app/src/main/java/com/univoice/feature/login/WelcomeActivity.kt
+++ app/src/main/java/com/univoice/feature/login/WelcomeActivity.kt
@@ -1,11 +1,6 @@
 package com.univoice.feature.login
 
 import android.content.Intent
-import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
-import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import com.univoice.R
 import com.univoice.core_ui.base.BindingActivity
 import com.univoice.databinding.ActivityWelcomeBinding
@@ -15,23 +10,7 @@ import dagger.hilt.android.AndroidEntryPoint
 @AndroidEntryPoint
 class WelcomeActivity : BindingActivity<ActivityWelcomeBinding>(R.layout.activity_welcome) {
     override fun initView() {
-        initToolbar()
-        initToolbarClickListener()
         initConfirmBtnClickListener()
-    }
-
-    private fun initToolbar() {
-        with(binding) {
-            setSupportActionBar(toolbar)
-            supportActionBar?.setDisplayShowTitleEnabled(false)
-            tvToolbarTitle.text = getString(R.string.login_toolbar_title)
-        }
-    }
-
-    private fun initToolbarClickListener() {
-        binding.toolbar.setNavigationOnClickListener {
-            finish()
-        }
     }
 
     private fun initConfirmBtnClickListener() {
