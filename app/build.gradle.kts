51d7b5db63d831415d9ec1e8e3bb669446642119
youjin09222 <youjin09222@gmail.com>
2024-07-10 15:19:16 +0900

    #19 [ADD]: circleIndicator 라이브러리 추가

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -82,6 +82,7 @@ dependencies {
     implementation(libs.coil)
     implementation(libs.viewPager2)
     implementation(libs.glide)
+    implementation(libs.circleindicator)
 
     // Hilt
     implementation(libs.dagger.hilt)
