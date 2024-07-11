0a5864e384f860847ce2cd9a909506b3c1333480
Eonji-sw <ejkim0625@gmail.com>
2024-07-11 18:00:33 +0900

    #15 [CHORE] : NoticeListEntity date -> start/end 구분

--- app/src/main/java/com/univoice/domain/entity/NoticeListEntity.kt
+++ app/src/main/java/com/univoice/domain/entity/NoticeListEntity.kt
@@ -3,7 +3,8 @@ package com.univoice.domain.entity
 data class NoticeListEntity(
     val subTitle: String,
     val title: String,
-    val date: String,
+    val startDate: String,
+    val endDate: String,
     val like: Int,
     val views: Int,
     val image: Boolean,
