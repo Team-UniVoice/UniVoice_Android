464b7e9a0bf537dece85fd977c2ab6f82b15bf6d
gaeulzzang <gaeulzzang@gmail.com>
2024-07-11 14:14:56 +0900

    #12 [REFACTOR] : DataStore 수정

--- app/src/main/java/com/univoice/data_local/UserPreferencesDataSourceImpl.kt
+++ app/src/main/java/com/univoice/data_local/UserPreferencesDataSourceImpl.kt
@@ -1,31 +1,33 @@
 package com.univoice.data_local
 
-import android.content.Context
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
 import androidx.datastore.preferences.core.edit
 import androidx.datastore.preferences.core.stringPreferencesKey
-import androidx.datastore.preferences.preferencesDataStore
 import com.univoice.data.datasource.UserPreferencesDataSource
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.map
+import javax.inject.Inject
 
-private val Context.dataStore by preferencesDataStore(name = "user_preferences")
-
-class UserPreferencesDataSourceImpl(private val context: Context) : UserPreferencesDataSource {
+class UserPreferencesDataSourceImpl @Inject constructor
+    (
+    private val dataStore: DataStore<Preferences>
+) : UserPreferencesDataSource {
     private val USER_ID = stringPreferencesKey("USER_ID")
     private val USER_PWD = stringPreferencesKey("USER_PWD")
 
     override suspend fun saveUserId(userId: String, userPwd: String) {
-        context.dataStore.edit { preferences ->
+        dataStore.edit { preferences ->
             preferences[USER_ID] = userId
             preferences[USER_PWD] = userPwd
         }
     }
 
-    override fun getUserId(): Flow<String?> = context.dataStore.data.map { preferences ->
+    override fun getUserId(): Flow<String?> = dataStore.data.map { preferences ->
         preferences[USER_ID]
     }
 
-    override fun getUserPwd(): Flow<String?> = context.dataStore.data.map { preferences ->
+    override fun getUserPwd(): Flow<String?> = dataStore.data.map { preferences ->
         preferences[USER_PWD]
     }
 }
--- app/src/main/java/com/univoice/di/UserPreferencesModule.kt
+++ app/src/main/java/com/univoice/di/UserPreferencesModule.kt
@@ -1,6 +1,9 @@
 package com.univoice.di
 
 import android.content.Context
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.preferencesDataStore
 import com.univoice.data.datasource.UserPreferencesDataSource
 import com.univoice.data.repositoryimpl.UserPreferencesRepositoryImpl
 import com.univoice.data_local.UserPreferencesDataSourceImpl
@@ -15,12 +18,20 @@ import javax.inject.Singleton
 @Module
 @InstallIn(SingletonComponent::class)
 object UserPreferencesModule {
+    private const val PREFERENCE_NAME = "user_preferences"
+    private val Context.dataStore by preferencesDataStore(name = PREFERENCE_NAME)
+
+    @Provides
+    @Singleton
+    fun provideDataStore(
+        @ApplicationContext context: Context
+    ): DataStore<Preferences> = context.dataStore
 
     @Provides
     @Singleton
     fun provideUserPreferencesDataSource(
-        @ApplicationContext context: Context
-    ): UserPreferencesDataSource = UserPreferencesDataSourceImpl(context)
+        dataStore: DataStore<Preferences>
+    ): UserPreferencesDataSource = UserPreferencesDataSourceImpl(dataStore)
 
     @Provides
     @Singleton
