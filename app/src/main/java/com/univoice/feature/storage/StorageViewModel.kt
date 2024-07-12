d067314b925164592fa3020723d42550c185b7fa
gaeulzzang <gaeulzzang@gmail.com>
2024-07-12 13:33:23 +0900

    #20 [ADD] mockStorageList 개수 늘리기

--- app/src/main/java/com/univoice/feature/storage/StorageViewModel.kt
+++ app/src/main/java/com/univoice/feature/storage/StorageViewModel.kt
@@ -53,5 +53,29 @@ class StorageViewModel : ViewModel() {
             viewCount = 100,
             avatar = "https://avatars.githubusercontent.com/u/91470334?s=400&u=4a743fda141cf8a074022b515b0ce3286e6c8560&v=4"
         ),
+        StorageEntity(
+            id = 7,
+            title = "명절 귀향 버스 수요 조사",
+            date = "06/26 ~ 07/01",
+            likeCount = 60,
+            viewCount = 100,
+            avatar = "https://avatars.githubusercontent.com/u/91470334?s=400&u=4a743fda141cf8a074022b515b0ce3286e6c8560&v=4"
+        ),
+        StorageEntity(
+            id = 8,
+            title = "명절 귀향 버스 수요 조사",
+            date = "06/26 ~ 07/01",
+            likeCount = 60,
+            viewCount = 100,
+            avatar = "https://avatars.githubusercontent.com/u/91470334?s=400&u=4a743fda141cf8a074022b515b0ce3286e6c8560&v=4"
+        ),
+        StorageEntity(
+            id = 9,
+            title = "명절 귀향 버스 수요 조사",
+            date = "06/26 ~ 07/01",
+            likeCount = 60,
+            viewCount = 100,
+            avatar = "https://avatars.githubusercontent.com/u/91470334?s=400&u=4a743fda141cf8a074022b515b0ce3286e6c8560&v=4"
+        ),
     )
 }
