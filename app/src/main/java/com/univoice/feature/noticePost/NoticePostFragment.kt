96264f6051b84105279594a4281ea71eec2c1fb4
Eonji-sw <ejkim0625@gmail.com>
2024-07-14 02:36:45 +0900

    #28 [FEAT] : NoticePostFragment photo 선택 구현

--- app/src/main/java/com/univoice/feature/noticePost/NoticePostFragment.kt
+++ app/src/main/java/com/univoice/feature/noticePost/NoticePostFragment.kt
@@ -1,15 +1,51 @@
 package com.univoice.feature.noticePost
 
+import android.Manifest
+import android.annotation.SuppressLint
+import android.net.Uri
+import android.os.Build
 import android.view.View
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.PickVisualMediaRequest
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.core.widget.doAfterTextChanged
 import androidx.fragment.app.setFragmentResultListener
+import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.univoice.R
 import com.univoice.core_ui.base.BindingFragment
+import com.univoice.core_ui.util.context.showPermissionAppSettingsDialog
 import com.univoice.databinding.FragmentNoticePostBinding
 
 class NoticePostFragment :
     BindingFragment<FragmentNoticePostBinding>(R.layout.fragment_notice_post) {
+    private val noticePostViewModel by viewModels<NoticePostViewModel>()
+
+    private lateinit var getGalleryLauncher: ActivityResultLauncher<String>
+    private lateinit var getPhotoPickerLauncher: ActivityResultLauncher<PickVisualMediaRequest>
+    private val requestPermissions =
+        registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted ->
+            when (isGranted) {
+                true -> {
+                    try {
+                        selectImage()
+                    } catch (_: Exception) {
+                    }
+                }
+
+                false -> handlePermissionDenied()
+            }
+        }
+    private var imageUris: MutableList<Uri> = mutableListOf()
+    private lateinit var imageAdapter: NoticePostImageAdapter
+
+    private fun handlePermissionDenied() {
+        if (!shouldShowRequestPermissionRationale(Manifest.permission.READ_MEDIA_IMAGES)) {
+            requireContext().showPermissionAppSettingsDialog()
+        }
+    }
+
     override fun initView() {
         initBackBtnClickListener()
         setTrackTitleTextChange()
@@ -17,10 +53,81 @@ class NoticePostFragment :
         setApplyBtnEnable()
         initApplyBtnClickListener()
 
+        initPhotoBtnClickListener()
+        initGalleryLauncher()
+        initPhotoPickerLauncher()
+        initPostImageAdapter()
+
         initTargetBtnClickListener()
         setOptionTarget()
         initOptionTargetClickListener()
         initOptionTargetDeleteBtnClickListener()
+    }
+
+    private fun initPostImageAdapter() {
+        imageAdapter = NoticePostImageAdapter(imageUris)
+        binding.rvNoticePostImage.layoutManager =
+            LinearLayoutManager(requireContext(), LinearLayoutManager.HORIZONTAL, false)
+        binding.rvNoticePostImage.adapter = imageAdapter
+    }
+
+    private fun initPhotoBtnClickListener() {
+        binding.layoutNoticePostPhotoBtn.setOnClickListener {
+            getGalleryPermission()
+        }
+    }
+
+    private fun getGalleryPermission() {
+        // API 34 이상인 경우
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.UPSIDE_DOWN_CAKE) {
+            selectImage()
+        } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            requestPermissions.launch(Manifest.permission.READ_MEDIA_IMAGES)
+        } else {
+            requestPermissions.launch(Manifest.permission.READ_EXTERNAL_STORAGE)
+        }
+    }
+
+    private fun selectImage() {
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.TIRAMISU) {
+            getGalleryLauncher.launch("image/*")
+        } else {
+            getPhotoPickerLauncher.launch(
+                PickVisualMediaRequest(ActivityResultContracts.PickVisualMedia.ImageOnly)
+            )
+        }
+    }
+
+    @SuppressLint("NotifyDataSetChanged")
+    private fun initPhotoPickerLauncher() {
+        getPhotoPickerLauncher =
+            registerForActivityResult(ActivityResultContracts.PickMultipleVisualMedia()) { imageUriData ->
+                imageUriData.let {
+                    if (imageUris.size + it.size <= 5) {
+                        imageUris.addAll(it)
+                        imageAdapter.notifyDataSetChanged()
+                        it.forEach { uri ->
+                            noticePostViewModel.setPhotoUri(uri.toString())
+                        }
+                    }
+                }
+            }
+    }
+
+    @SuppressLint("NotifyDataSetChanged")
+    private fun initGalleryLauncher() {
+        getGalleryLauncher =
+            registerForActivityResult(ActivityResultContracts.GetMultipleContents()) { imageUriData ->
+                imageUriData.let {
+                    if (imageUris.size + it.size <= 5) {
+                        imageUris.addAll(it)
+                        imageAdapter.notifyDataSetChanged()
+                        it.forEach { uri ->
+                            noticePostViewModel.setPhotoUri(uri.toString())
+                        }
+                    }
+                }
+            }
     }
 
     private fun setTrackTitleTextChange() {
@@ -58,7 +165,6 @@ class NoticePostFragment :
             binding.layoutNoticePostOptionTarget.visibility = View.VISIBLE
             binding.tvNoticePostOptionTargetContent.text =
                 bundle.getString(NOTICE_POST_TARGET_BOTTOM_SHEET_ARGS_CONTENT)
-            binding.spaceNoticePostOptionFirst.visibility = View.VISIBLE
         }
     }
 
@@ -94,7 +200,8 @@ class NoticePostFragment :
 
     companion object {
         const val NOTICE_POST_TARGET_BOTTOM_SHEET = "notice_post_target_bottom_sheet"
-        const val NOTICE_POST_TARGET_BOTTOM_SHEET_ARGS = "notice_post_target_bottom_sheet_args"
+        const val NOTICE_POST_TARGET_BOTTOM_SHEET_ARGS =
+            "notice_post_target_bottom_sheet_args"
         const val NOTICE_POST_TARGET_BOTTOM_SHEET_ARGS_CONTENT =
             "notice_post_target_bottom_sheet_args_content"
     }
