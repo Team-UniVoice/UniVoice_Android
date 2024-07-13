7f8c35f0e268d35341139a72506473b508168217
Eonji-sw <ejkim0625@gmail.com>
2024-07-14 02:58:40 +0900

    #28 [FEAT] : NoticePostImage ItemDecorator 구현

---
+++ app/src/main/java/com/univoice/feature/noticePost/NoticePostImageItemDecorator.kt
@@ -0,0 +1,29 @@
+package com.univoice.feature.noticePost
+
+import android.content.Context
+import android.graphics.Rect
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+import com.univoice.core_ui.util.context.pxToDp
+
+class NoticePostImageItemDecorator(val context: Context) : RecyclerView.ItemDecoration() {
+    override fun getItemOffsets(
+        outRect: Rect,
+        view: View,
+        parent: RecyclerView,
+        state: RecyclerView.State,
+    ) {
+        super.getItemOffsets(outRect, view, parent, state)
+        val position = parent.getChildAdapterPosition(view)
+        val itemCount = parent.adapter?.itemCount ?: 0
+
+        if (position == 0) {
+            outRect.left = context.pxToDp(16)
+            outRect.right = context.pxToDp(12)
+        } else if (position == itemCount - 1) {
+            outRect.right = context.pxToDp(16)
+        } else {
+            outRect.right = context.pxToDp(12)
+        }
+    }
+}
