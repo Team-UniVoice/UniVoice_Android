374e56c45572082e716cd89003ccb0c74d661c36
Eonji-sw <ejkim0625@gmail.com>
2024-07-16 21:52:41 +0900

    #42 [FEAT] : login API 연결

--- app/src/main/java/com/univoice/feature/login/LoginViewModel.kt
+++ app/src/main/java/com/univoice/feature/login/LoginViewModel.kt
@@ -1,10 +1,10 @@
 package com.univoice.feature.login
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.univoice.domain.repository.UserPreferencesRepository
+import com.univoice.core_ui.view.UiState
+import com.univoice.domain.repository.LoginRepository
+import com.univoice.domain.repository.UserInfoRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
@@ -13,32 +13,29 @@ import javax.inject.Inject
 
 @HiltViewModel
 class LoginViewModel @Inject constructor(
-    private val userPreferencesRepository: UserPreferencesRepository
+    private val loginRepository: LoginRepository,
+    private val userInfoRepository: UserInfoRepository
 ) : ViewModel() {
-    private val _loginState = MutableLiveData<Boolean>()
-    val loginState: LiveData<Boolean> = _loginState
+    private val _postLoginState = MutableStateFlow<UiState<String>>(UiState.Empty)
+    val postLoginState: StateFlow<UiState<String>> = _postLoginState
 
-    private val _userId = MutableStateFlow<String?>(null)
-    val userId: StateFlow<String?> = _userId
-
-    private val _userPwd = MutableStateFlow<String?>(null)
-    val userPwd: StateFlow<String?> = _userPwd
-
-    init {
-        viewModelScope.launch {
-            userPreferencesRepository.getUserId().collect {
-                _userId.value = it
-            }
-            userPreferencesRepository.getUserPwd().collect {
-                _userPwd.value = it
-            }
-        }
-        _loginState.value = true
+    fun postLogin(email: String, password: String) = viewModelScope.launch {
+        _postLoginState.emit(UiState.Loading)
+        loginRepository.postLogin(email, password).fold(
+            {
+                if (it != null) _postLoginState.emit(UiState.Success(it)) else _postLoginState.emit(
+                    UiState.Failure("400")
+                )
+            },
+            { _postLoginState.emit(UiState.Failure(it.message.toString())) }
+        )
     }
 
-    fun saveUserCredentials(userId: String, userPwd: String) {
+    fun getUserAccessToken() = userInfoRepository.getUserAccessToken()
+
+    fun saveUserAccessToken(accessToken: String) {
         viewModelScope.launch {
-            userPreferencesRepository.saveUserId(userId, userPwd)
+            userInfoRepository.saveUserAccessToken(accessToken)
         }
     }
 }
