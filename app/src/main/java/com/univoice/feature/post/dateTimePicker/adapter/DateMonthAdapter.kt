c0e7c677f95868aa6c842ca8fc8b7c4a4704f664
youjin <youjin09222@gmail.com>
2024-07-17 06:16:35 +0900

    #27 [FEAT]: DateMonthAdapter 구현

---
+++ app/src/main/java/com/univoice/feature/post/dateTimePicker/adapter/DateMonthAdapter.kt
@@ -0,0 +1,41 @@
+package com.univoice.feature.post.dateTimePicker.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.recyclerview.widget.RecyclerView
+import com.univoice.R
+import java.util.Calendar
+
+class DateMonthAdapter(
+    val months: ArrayList<Calendar>,
+    private val dividerHeight: Int
+) :
+    RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        val view = DateViewHolder(
+            DataBindingUtil.inflate(
+                LayoutInflater.from(parent.context),
+                R.layout.item_date_number,
+                parent,
+                false
+            )
+        )
+        view.binding.layoutDateNumber.layoutParams.height =
+            dividerHeight.dpToPx(view.binding.tvDateNumber.context).toInt()
+        return view
+    }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        val viewHolder = holder as DateViewHolder
+
+        viewHolder.binding.tvDateNumber.text = getMonthFromCalendar(months[position])
+    }
+
+    override fun getItemCount() = months.size
+
+    override fun getItemId(position: Int): Long {
+        return position.toLong()
+    }
+}
