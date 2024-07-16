fade62ec9f857d30fa0f1a3c0fdb9afaca80ccea
Eonji-sw <ejkim0625@gmail.com>
2024-07-17 03:37:45 +0900

    #42 [FEAT] : 자동 로그인 구현

--- app/src/main/java/com/univoice/domain/repository/UserInfoRepository.kt
+++ app/src/main/java/com/univoice/domain/repository/UserInfoRepository.kt
@@ -6,4 +6,10 @@ interface UserInfoRepository {
     suspend fun saveUserAccessToken(accessToken: String)
 
     fun getUserAccessToken(): Flow<String?>
+
+    suspend fun saveCheckLogin(checkLogin: Boolean)
+
+    fun getCheckLogin(): Flow<Boolean>
+
+    suspend fun clear()
 }
