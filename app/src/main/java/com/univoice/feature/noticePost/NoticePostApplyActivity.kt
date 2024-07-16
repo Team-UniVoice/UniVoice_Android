ba6624de347ff0080cbffbf539b9e94025810704
gaeulzzang <gaeulzzang@gmail.com>
2024-07-17 03:04:33 +0900

    #47 [UI] 공지 등록 화면 노티 로직 및 저장뷰 패딩 조정

--- app/src/main/java/com/univoice/feature/noticePost/NoticePostApplyActivity.kt
+++ app/src/main/java/com/univoice/feature/noticePost/NoticePostApplyActivity.kt
@@ -1,21 +1,44 @@
 package com.univoice.feature.noticePost
 
-import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
-import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import android.animation.Animator
+import android.content.Intent
+import android.view.View
 import com.univoice.R
+import com.univoice.core_ui.base.BindingActivity
+import com.univoice.databinding.ActivityNoticePostApplyBinding
+import com.univoice.feature.MainActivity
 
-class NoticePostApplyActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_notice_post_apply)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+class NoticePostApplyActivity :
+    BindingActivity<ActivityNoticePostApplyBinding>(R.layout.activity_notice_post_apply) {
+    override fun initView() {
+        initLottieAnimation()
+        initConfirmBtnClickListener()
+    }
+
+    private fun initLottieAnimation() {
+        binding.lottieNoticePostApply.apply {
+            playAnimation()
+            addAnimatorListener(object : Animator.AnimatorListener {
+                override fun onAnimationStart(animation: Animator) {}
+
+                override fun onAnimationEnd(animation: Animator) {
+                    binding.btnNoticePostApplyConfirm.visibility = View.VISIBLE
+                }
+
+                override fun onAnimationCancel(animation: Animator) {}
+
+                override fun onAnimationRepeat(animation: Animator) {}
+            })
         }
     }
+
+    private fun initConfirmBtnClickListener() {
+        binding.btnNoticePostApplyConfirm.setOnClickListener {
+            Intent(this, MainActivity::class.java).apply {
+                flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+                startActivity(this)
+            }
+        }
+    }
+
 }
