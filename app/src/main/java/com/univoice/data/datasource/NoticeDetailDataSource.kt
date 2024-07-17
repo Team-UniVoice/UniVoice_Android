c3f8618ae82d83ab06d61eccc1dd8a022290b95e
youjin <youjin09222@gmail.com>
2024-07-18 05:36:07 +0900

    #85 [FEAT]: Like DataSource 구현

--- app/src/main/java/com/univoice/data/datasource/NoticeDetailDataSource.kt
+++ app/src/main/java/com/univoice/data/datasource/NoticeDetailDataSource.kt
@@ -5,4 +5,8 @@ import com.univoice.data.dto.response.ResponseNoticeDetailDto
 
 interface NoticeDetailDataSource {
     suspend fun getNoticeDetail(noticeId: Int): BaseResponse<ResponseNoticeDetailDto>
+
+    suspend fun postNoticeLike(noticeId: Int): BaseResponse<Unit>
+
+    suspend fun postNoticeDelLike(noticeId: Int): BaseResponse<Unit>
 }
