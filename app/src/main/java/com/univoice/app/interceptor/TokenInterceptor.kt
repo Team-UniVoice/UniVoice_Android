90a056e718ed2d257c20ae3cd781ead3ded79b96
Eonji-sw <ejkim0625@gmail.com>
2024-07-17 09:58:38 +0900

    #42 [MOD] : 불필요한 코드 수정

--- app/src/main/java/com/univoice/app/interceptor/TokenInterceptor.kt
+++ app/src/main/java/com/univoice/app/interceptor/TokenInterceptor.kt
@@ -1,6 +1,8 @@
 package com.univoice.app.interceptor
 
 import com.univoice.data.datasource.UserPreferencesDataSource
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.runBlocking
 import okhttp3.Interceptor
 import okhttp3.Response
 import timber.log.Timber
@@ -10,15 +12,16 @@ class TokenInterceptor @Inject
 constructor(
     private val userPreferencesDataSource: UserPreferencesDataSource,
 ) : Interceptor {
-    override fun intercept(chain: Interceptor.Chain): Response {
-        var accessToken = userPreferencesDataSource.getUserAccessToken()
+    override fun intercept(chain: Interceptor.Chain): Response = runBlocking {
+        var accessToken = userPreferencesDataSource.getUserAccessToken().first()
 
-        // 기존 request
-        val request =
-            chain.request().newBuilder().addHeader("Authorization", "Bearer $accessToken").build()
+        val request = chain.request().newBuilder()
+            .addHeader("Authorization", "Bearer $accessToken")
+            .build()
+
         val response = chain.proceed(request)
         Timber.tag("interceptor").d("accessToken $accessToken")
 
-        return response
+        response
     }
 }
