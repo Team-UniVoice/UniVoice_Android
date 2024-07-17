f6ec591e08ec1a141a86cb8e4a4c56d738f668af
youjin <youjin09222@gmail.com>
2024-07-18 05:35:43 +0900

    #85 [FEAT]: Like Api Service 구현

--- app/src/main/java/com/univoice/data_remote/api/NoticeDetailApiService.kt
+++ app/src/main/java/com/univoice/data_remote/api/NoticeDetailApiService.kt
@@ -3,10 +3,13 @@ package com.univoice.data_remote.api
 import com.univoice.data.dto.BaseResponse
 import com.univoice.data.dto.response.ResponseNoticeDetailDto
 import com.univoice.data_remote.api.ApiKeyStorage.API
+import com.univoice.data_remote.api.ApiKeyStorage.CANCEL
+import com.univoice.data_remote.api.ApiKeyStorage.LIKE
 import com.univoice.data_remote.api.ApiKeyStorage.NOTICE
 import com.univoice.data_remote.api.ApiKeyStorage.NOTICE_ID
 import com.univoice.data_remote.api.ApiKeyStorage.V1
 import retrofit2.http.GET
+import retrofit2.http.POST
 import retrofit2.http.Path
 
 interface NoticeDetailApiService {
@@ -14,4 +17,14 @@ interface NoticeDetailApiService {
     suspend fun getNoticeDetail(
         @Path(NOTICE_ID) noticeId: Int
     ): BaseResponse<ResponseNoticeDetailDto>
+
+    @POST("/$API/$V1/$NOTICE/$LIKE/{$NOTICE_ID}")
+    suspend fun postNoticeLike(
+        @Path(NOTICE_ID) noticeId: Int
+    ): BaseResponse<Unit>
+
+    @POST("/$API/$V1/$NOTICE/$LIKE/$CANCEL/{$NOTICE_ID}")
+    suspend fun postNoticeDelLike(
+        @Path(NOTICE_ID) noticeId: Int
+    ): BaseResponse<Unit>
 }
