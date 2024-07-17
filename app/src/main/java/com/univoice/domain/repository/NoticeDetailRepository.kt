456f24006b3406808b9fd51426a6af968c77c95d
youjin <youjin09222@gmail.com>
2024-07-18 05:36:39 +0900

    #85 [FEAT]: Like Repository 구현

--- app/src/main/java/com/univoice/domain/repository/NoticeDetailRepository.kt
+++ app/src/main/java/com/univoice/domain/repository/NoticeDetailRepository.kt
@@ -4,4 +4,8 @@ import com.univoice.domain.entity.NoticeDetailEntity
 
 interface NoticeDetailRepository {
     suspend fun getNoticeDetail(noticeId: Int): Result<NoticeDetailEntity>
+
+    suspend fun postNoticeLike(noticeId: Int): Result<Unit>
+
+    suspend fun postNoticeDisLike(noticeId: Int): Result<Unit>
 }
