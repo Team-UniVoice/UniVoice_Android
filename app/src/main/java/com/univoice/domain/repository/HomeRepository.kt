52c55f186541bb9a04dea5eff6b032d68b9730e1
Eonji-sw <ejkim0625@gmail.com>
2024-07-17 19:05:06 +0900

    #61 [FEAT] : HomeNotice university, college, department Repository 구현

--- app/src/main/java/com/univoice/domain/repository/HomeRepository.kt
+++ app/src/main/java/com/univoice/domain/repository/HomeRepository.kt
@@ -7,4 +7,10 @@ interface HomeRepository {
     suspend fun getNoticeQuickScan(): Result<List<QuickScanListEntity>?>
 
     suspend fun getNoticeAll(): Result<List<NoticeListEntity>?>
+
+    suspend fun getNoticeUniversity(): Result<List<NoticeListEntity>?>
+
+    suspend fun getNoticeCollege(): Result<List<NoticeListEntity>?>
+
+    suspend fun getNoticeDepartment(): Result<List<NoticeListEntity>?>
 }
