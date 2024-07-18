dd752dd3fe61b951d9242a9fcd30de18e09f6867
gaeulzzang <gaeulzzang@gmail.com>
2024-07-19 08:47:17 +0900

    #107 [FEAT] : 멀티파트 완서어어엉엉

--- app/src/main/java/com/univoice/feature/noticePost/NoticePostFragment.kt
+++ app/src/main/java/com/univoice/feature/noticePost/NoticePostFragment.kt
@@ -3,13 +3,11 @@ package com.univoice.feature.noticePost
 import android.Manifest
 import android.annotation.SuppressLint
 import android.content.Context
-import android.database.Cursor
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
-import android.graphics.Matrix
 import android.net.Uri
 import android.os.Build
-import android.provider.MediaStore
+import android.util.Log
 import android.view.View
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.PickVisualMediaRequest
@@ -19,10 +17,9 @@ import androidx.core.widget.doAfterTextChanged
 import androidx.fragment.app.setFragmentResultListener
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.flowWithLifecycle
-import androidx.navigation.NavOptions
+import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
-import coil.decode.DecodeUtils.calculateInSampleSize
 import com.univoice.R
 import com.univoice.core_ui.base.BindingFragment
 import com.univoice.core_ui.util.context.showPermissionAppSettingsDialog
@@ -34,6 +31,7 @@ import com.univoice.feature.post.dateTimePicker.TimeBottomSheetFragment
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.launch
 import timber.log.Timber
 import java.io.ByteArrayOutputStream
 import java.io.File
@@ -112,14 +110,14 @@ class NoticePostFragment :
     private fun setupPostNoticeObserve() {
         noticePostViewModel.postNoticeState.flowWithLifecycle(viewLifeCycle).onEach {
             when (it) {
-                is UiState.Loading -> Timber.tag("NoticePost").d("로딩")
+                is UiState.Loading -> Timber.tag("signup").d("로딩")
                 is UiState.Success -> findNavController().navigate(
                     R.id.action_fragment_notice_post_to_fragment_notice_post_apply,
-                    null, NavOptions.Builder().setPopUpTo(R.id.fragment_notice_post, true).build()
+                    null
                 )
 
-                is UiState.Failure -> Timber.tag("NoticePost").d(it.msg)
-                is UiState.Empty -> Timber.tag("NoticePost").d("비었음")
+                is UiState.Failure -> Timber.tag("signup").d(it.msg)
+                is UiState.Empty -> Timber.tag("signup").d("비었음")
             }
         }.launchIn(viewLifeCycleScope)
     }
@@ -158,14 +156,20 @@ class NoticePostFragment :
 
                 tvNoticePostOptionDateStart.text = bundle.getString(SET_START_DATE, "")
                 tvNoticePostOptionDateEnd.text = bundle.getString(SET_END_DATE, "")
-                tvNoticePostOptionTimeStart.text = bundle.getString(SET_START_TIME, "")
-                tvNoticePostOptionTimeEnd.text = bundle.getString(SET_END_TIME, "")
+
                 hereStartDate = bundle.getString(SET_START_DATE, "")
                 hereEndData = bundle.getString(SET_END_DATE, "")
                 hereStartTime = bundle.getString(SET_START_TIME, "")
                 hereEndTime = bundle.getString(SET_END_TIME, "")
                 allDayCheck = bundle.getBoolean(CLICK_BUTTON)
 
+                if (allDayCheck) {
+                    binding.tvNoticePostOptionTimeStart.visibility = View.GONE
+                    binding.tvNoticePostOptionTimeEnd.visibility = View.GONE
+                } else {
+                    binding.tvNoticePostOptionTimeStart.visibility = View.VISIBLE
+                    binding.tvNoticePostOptionTimeEnd.visibility = View.VISIBLE
+                }
 
                 layoutNoticePostOptionDate.visibility = View.VISIBLE
 
@@ -190,25 +194,32 @@ class NoticePostFragment :
         }
     }
 
+    // "2024년7월8일(목)"로 들어오면 "2024년 7월 8일 오전 00시 00분" 로 반환
     private fun convertToFullDateFormat(inputDate: String): String {
+        // 정규식을 사용하여 날짜 부분과 요일 부분을 분리
         val datePattern = Regex("""(\d{4})년(\d{1,2})월(\d{1,2})일\(\p{IsAlphabetic}+\)""")
         val matchResult = datePattern.find(inputDate)
 
         return if (matchResult != null) {
             val (year, month, day) = matchResult.destructured
+            // 새로운 형식으로 변환
             "${year}년 ${month}월 ${day}일 오전 00시 00분"
         } else {
+            // 매칭되지 않을 경우 원본 문자열 반환
             inputDate
         }
     }
 
+    // 유진언니가 UI 시, 분 텍스트 추가하면 없애야 함!! : "6월 6일(목) 오후 12:25" 로 들어오면 "2024년 6월 6일 오후 12시 25분" 로 반환
     private fun formatDateTime(input: String): String {
+        // 정규식을 사용하여 날짜와 시간을 추출
         val regex = Regex("(\\d{1,2})월 (\\d{1,2})일\\(\\S+\\) (오전|오후) (\\d{1,2}):(\\d{2})")
         val matchResult = regex.find(input)
 
         return if (matchResult != null) {
             val (month, day, period, hour, minute) = matchResult.destructured
 
+            // 시간 부분을 변환
             val formattedHour = if (period == "오후" && hour.toInt() != 12) {
                 hour.toInt() + 12
             } else if (period == "오전" && hour.toInt() == 12) {
@@ -217,10 +228,13 @@ class NoticePostFragment :
                 hour.toInt()
             }
 
+            // 최종 시간 표현
             val finalHour = if (formattedHour == 0) 12 else formattedHour
 
+            // 원하는 형식으로 조립
             "2024년 ${month}월 ${day}일 ${period} ${finalHour}시 ${minute}분"
         } else {
+            // 매칭되지 않으면 원래 문자열 반환
             input
         }
     }
@@ -273,9 +287,14 @@ class NoticePostFragment :
         getPhotoPickerLauncher =
             registerForActivityResult(ActivityResultContracts.PickMultipleVisualMedia()) { imageUriData ->
                 imageUriData.let {
-                    if (imageUris.size + it.size <= 5) {
-                        imageUris.addAll(it)
-                        imageAdapter.notifyDataSetChanged()
+                    lifecycleScope.launch {
+                        val compressedUris = it.mapNotNull { uri ->
+                            compressImageUri(requireContext(), uri)
+                        }
+                        if (imageUris.size + compressedUris.size <= 5) {
+                            imageUris.addAll(compressedUris)
+                            imageAdapter.notifyDataSetChanged()
+                        }
                     }
                 }
             }
@@ -286,9 +305,14 @@ class NoticePostFragment :
         getGalleryLauncher =
             registerForActivityResult(ActivityResultContracts.GetMultipleContents()) { imageUriData ->
                 imageUriData.let {
-                    if (imageUris.size + it.size <= 5) {
-                        imageUris.addAll(it)
-                        imageAdapter.notifyDataSetChanged()
+                    lifecycleScope.launch {
+                        val compressedUris = it.mapNotNull { uri ->
+                            compressImageUri(requireContext(), uri)
+                        }
+                        if (imageUris.size + compressedUris.size <= 5) {
+                            imageUris.addAll(compressedUris)
+                            imageAdapter.notifyDataSetChanged()
+                        }
                     }
                 }
             }
@@ -317,26 +341,36 @@ class NoticePostFragment :
                 sendTarget = null
             }
 
-            noticePostViewModel.postNotice(
-                title = binding.etNoticePostTitle.text.toString(),
-                content = binding.etNoticePostContent.text.toString(),
-                target = sendTarget,
-                startTime = sendStartTotalTime?.let { it -> convertToISO8601(it) },
-                endTime = sendEndTotalTime?.let { it -> convertToISO8601(it) },
-                noticeImages = convertUriListToFileList(requireContext(), imageUris)
-            )
+            lifecycleScope.launch {
+                val compressedImages = compressImageUris(requireContext(), imageUris)
+                compressedImages.forEach { file ->
+                    Log.d("ImageCompression", "Compressed image size: ${file.length()} bytes")
+                }
+                noticePostViewModel.postNotice(
+                    title = binding.etNoticePostTitle.text.toString(),
+                    content = binding.etNoticePostContent.text.toString(),
+                    target = sendTarget,
+                    startTime = sendStartTotalTime?.let { it -> convertToISO8601(it) },
+                    endTime = sendEndTotalTime?.let { it -> convertToISO8601(it) },
+                    noticeImages = compressedImages
+                )
+            }
         }
     }
 
     @SuppressLint("SimpleDateFormat")
     private fun convertToISO8601(inputDate: String): String? {
+        // 한국어 날짜 형식
         val koreanDateFormat = SimpleDateFormat("yyyy년 M월 d일 a h시 m분", Locale.KOREAN)
 
+        // ISO 8601 날짜 형식
         val iso8601Format = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'")
         iso8601Format.timeZone = TimeZone.getTimeZone("UTC")
 
         return try {
+            // 입력된 한국어 날짜를 파싱
             val date = koreanDateFormat.parse(inputDate)
+            // ISO 8601 형식으로 변환하여 반환
             iso8601Format.format(date)
         } catch (e: Exception) {
             e.printStackTrace()
@@ -344,49 +378,96 @@ class NoticePostFragment :
         }
     }
 
-    private fun convertUriListToFileList(context: Context, uriList: MutableList<Uri>): List<File> {
+    private suspend fun compressImageUris(context: Context, uriList: MutableList<Uri>): List<File> {
         return uriList.mapNotNull { uri ->
-            val originalFile = uriToFile(context, uri)
-            originalFile?.let { compressImageFile(it) }
+            val bitmap = decodeSampledBitmapFromUri(context, uri, 512, 512)
+            val compressedBitmap = compressBitmapToMaxSize(bitmap, 1024 * 1024)
+            saveBitmapToFile(context, compressedBitmap)
         }
     }
 
-    private fun uriToFile(context: Context, uri: Uri): File? {
-        val projection = arrayOf(MediaStore.Images.Media.DATA)
-        val cursor: Cursor? = context.contentResolver.query(uri, projection, null, null, null)
-        cursor?.use {
-            if (it.moveToFirst()) {
-                val columnIndex = it.getColumnIndexOrThrow(MediaStore.Images.Media.DATA)
-                val filePath = it.getString(columnIndex)
-                return File(filePath)
+    private suspend fun compressImageUri(context: Context, uri: Uri): Uri? {
+        val bitmap = decodeSampledBitmapFromUri(context, uri, 512, 512)
+        val compressedBitmap = compressBitmapToMaxSize(bitmap, 1024 * 1024)
+        return saveBitmapToUri(context, compressedBitmap)
+    }
+
+    private fun decodeSampledBitmapFromUri(context: Context, uri: Uri, reqWidth: Int, reqHeight: Int): Bitmap {
+        val options = BitmapFactory.Options().apply {
+            inJustDecodeBounds = true
+        }
+
+        context.contentResolver.openInputStream(uri)?.use {
+            BitmapFactory.decodeStream(it, null, options)
+        }
+
+        options.inSampleSize = calculateInSampleSize(options, reqWidth, reqHeight)
+
+        options.inJustDecodeBounds = false
+        return context.contentResolver.openInputStream(uri)?.use {
+            BitmapFactory.decodeStream(it, null, options)!!
+        } ?: throw IOException("Unable to decode bitmap from URI")
+    }
+
+    private fun calculateInSampleSize(options: BitmapFactory.Options, reqWidth: Int, reqHeight: Int): Int {
+        val (height: Int, width: Int) = options.run { outHeight to outWidth }
+        var inSampleSize = 1
+
+        if (height > reqHeight || width > reqWidth) {
+            val halfHeight: Int = height / 2
+            val halfWidth: Int = width / 2
+
+            while (halfHeight / inSampleSize >= reqHeight && halfWidth / inSampleSize >= reqWidth) {
+                inSampleSize *= 2
             }
         }
-        return null
+
+        return inSampleSize
     }
 
-    private fun compressImageFile(file: File): File {
-        var bitmap = BitmapFactory.decodeFile(file.path)
-        var quality = 100
-        val maxFileSize = 5 * 1024 * 1024 // 5MB
-        val byteArrayOutputStream = ByteArrayOutputStream()
+    private fun compressBitmapToMaxSize(bitmap: Bitmap, maxSize: Long): Bitmap {
+        var compressRate = 100
+        var compressedImageSize: Long
+        var resultBitmap = bitmap
 
-        while (true) {
-            bitmap.compress(Bitmap.CompressFormat.JPEG, quality, byteArrayOutputStream)
-            if (byteArrayOutputStream.size() <= maxFileSize) break
-            quality -= 5
-        }
+        do {
+            val outputStream = ByteArrayOutputStream()
+            bitmap.compress(Bitmap.CompressFormat.JPEG, compressRate, outputStream)
+            val compressedImage = outputStream.toByteArray()
+            compressedImageSize = compressedImage.size.toLong()
 
-        val compressedFile = File(file.parent, "compressed_${file.name}")
-        FileOutputStream(compressedFile).use { out ->
-            out.write(byteArrayOutputStream.toByteArray())
-        }
+            if (compressedImageSize > maxSize) {
+                compressRate -= 5
+            } else {
+                resultBitmap = BitmapFactory.decodeByteArray(compressedImage, 0, compressedImage.size)
+            }
+        } while (compressedImageSize > maxSize && compressRate > 0)
 
-        return compressedFile
+        Log.d("ImageCompression", "Compressed image size: ${bytesToMegabytes(compressedImageSize)} MB")
+        return resultBitmap
+    }
+
+    private fun saveBitmapToFile(context: Context, bitmap: Bitmap): File {
+        val file = File(context.externalCacheDir, "compressed_image_${System.currentTimeMillis()}.jpg")
+        val fileOutputStream = FileOutputStream(file)
+        bitmap.compress(Bitmap.CompressFormat.JPEG, 100, fileOutputStream)
+        fileOutputStream.flush()
+        fileOutputStream.close()
+        return file
+    }
+
+    private fun saveBitmapToUri(context: Context, bitmap: Bitmap): Uri? {
+        val file = saveBitmapToFile(context, bitmap)
+        return Uri.fromFile(file)
+    }
+
+    private fun bytesToMegabytes(bytes: Long): Double {
+        return bytes / (1024.0 * 1024.0)
     }
 
     private fun setApplyBtnEnable() {
         with(binding) {
-            if (etNoticePostTitle.text.isNotBlank() && etNoticePostContent.text.isNotBlank()) {
+            if (etNoticePostTitle.text.isNotEmpty() && etNoticePostContent.text.isNotEmpty()) {
                 btnToolbarNoticePostApply.setBackgroundResource(R.drawable.shape_mint400_fill_20_rect)
                 btnToolbarNoticePostApply.isEnabled = true
             } else {
