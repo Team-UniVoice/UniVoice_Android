36715705b7ebcf3f3fedd89e0d203f82d58618a0
twogarlic <hanle0126@naver.com>
2024-07-19 20:30:03 +0900

    #117 [MOD] 중복확인 로직 수정

--- app/src/main/java/com/univoice/feature/signup/SignUpActivity.kt
+++ app/src/main/java/com/univoice/feature/signup/SignUpActivity.kt
@@ -21,7 +21,7 @@ class SignUpActivity : BindingActivity<ActivitySignupBinding>(R.layout.activity_
 
     private fun initStartBtnClickListener() {
         binding.btnSignupStart.setOnClickListener {
-            startActivity(Intent(this, CreateAccountActivity::class.java))
+            startActivity(Intent(this, SchoolInputActivity::class.java))
         }
     }
 }
